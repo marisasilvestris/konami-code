d485297afcf69a64462d2ac48133d92e90654844
Marisa Silvestris <marisasilvestris@gmail.com>
2026-02-18 16:52:23 +0000

    pupdate - keysequence and callback

--- konami.js
+++ konami.js
@@ -1,22 +1,22 @@
-function konamiCode() {
-  const keySequence = [
-    "ArrowUp",
-    "ArrowUp",
-    "ArrowDown",
-    "ArrowDown",
-    "ArrowLeft",
-    "ArrowRight",
-    "ArrowLeft",
-    "ArrowRight",
-    "b",
-    "a",
-  ];
+export default function konamiCode(keySequence, fireEvent) {
+  // const keySequence = [
+  //   "ArrowUp",
+  //   "ArrowUp",
+  //   "ArrowDown",
+  //   "ArrowDown",
+  //   "ArrowLeft",
+  //   "ArrowRight",
+  //   "ArrowLeft",
+  //   "ArrowRight",
+  //   "b",
+  //   "a",
+  // ];
   let y = 0;
   document.addEventListener(`keydown`, (keypress) => {
     if (keypress.key === keySequence[y] && y < keySequence.length) {
       y++;
       if (y == keySequence.length) {
-        console.log(`you win!`);
+        fireEvent();
       }
     } else {
       y = 0;
@@ -24,6 +24,6 @@ function konamiCode() {
   });
 }
 
-konamiCode();
-
+// keySequence can easily be edited with the aid of https://keyjs.dev/
+// Import this file and call konamiCode([keySequence], {callBack}), with the function to execute when input is successful.
 // loosely adapted from the real Konami Code in Contra, courtesy of Displaced Gamers' codewalk - https://youtu.be/8LnwsYL7Apk?si=34dtXQ9eJZf2uLgu&t=379
